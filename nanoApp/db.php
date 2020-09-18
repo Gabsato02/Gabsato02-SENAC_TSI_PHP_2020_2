0a3848e0ddf9c01bb2e5e110d4b43568c8d61b7b
Gabsato02 <gabsato02@gmail.com>
2020-09-17 22:36:42 -0300

    Exemplos de como usar PDO para conexão ao banco de dados + método POST

---
+++ nanoApp/db.php
@@ -0,0 +1,13 @@
+<?php
+
+require_once "config.php";
+
+// Tente X comando
+try {
+    $objDatabase = new PDO(DSN, DBUSER, DBPASSWORD);
+} 
+// Caso dê erro retorna a classe de exceção PDOException sendo instanciada no $objErro
+catch (PDOException $objError) {
+    // Usando o método getMessage pra receber a mensagem de erro
+    echo 'SGBD indisponível - Erro ' . $objError -> getMessage();
+}
